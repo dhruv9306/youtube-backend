9b0e53c9e3b6065f36c2f88999abaa9b541cb317
Dhruv Gandhi <dhruvgandhi870842@gmail.com>
2024-03-13 22:59:14 +0530

    suc

---
+++ src/index.js
@@ -0,0 +1,50 @@
+// require('dotenv').config({path:'./env'})
+
+import dotenv from "dotenv"
+
+// import mongoose from "mongoose";
+
+// import {DB_NAME} from "./constants";
+
+import connectDb from "./db/index.js";
+
+
+dotenv.config({
+    path:'./env'
+})
+
+connectDb()
+
+
+
+
+
+
+
+
+
+
+
+// import express from "express";
+// const app = express();
+
+// ( async ()=>{
+//     try{
+//         await mongoose .connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+//         app.on("error", (error)=>{
+//             console.log("err:",error);
+//             throw error;
+//         })
+
+//         app.listen(process.env.PORT,()=>{
+//             console.log(`listening on port ${process.env.PORT}`);
+
+//         })
+
+//     }
+//     catch (error){
+//         console.log("error:",error);
+//         throw err
+//     }
+
+// }) ()
