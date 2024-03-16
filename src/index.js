a60877363712cc836161d1443e3014f3f9c2ad42
Dhruv Gandhi <dhruvgandhi870842@gmail.com>
2024-03-16 19:43:32 +0530

    successfully done

--- src/index.js
+++ src/index.js
@@ -14,6 +14,15 @@ dotenv.config({
 })
 
 connectDb()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`server is running on port${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("mongodb connnection failed",err);
+
+})
 
 
 
