c683632246523b6b4d218ac9e9cbccb598806715
Dhruv Gandhi <dhruvgandhi870842@gmail.com>
2024-04-01 00:22:39 +0530

    postman updation processing

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,6 @@
 import {asyncHandler} from "../utils/asyncHandler.js"
 import {ApiError} from "../utils/apiError.js"
-import {User} from "..models/user.model.js"
+import {User} from "../models/user.model.js"
 import { uploadoncloudinary } from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
 
@@ -19,7 +19,7 @@ const registerUser= asyncHandler( async(req,res)=>{
 
     }
 
-    const existedUser= User.findOne({
+    const existedUser= await User.findOne({
         $or: [{username},{email}]
     })
 
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,17 +1,13 @@
-import {vs as cloudinary} from "cloudinary";
+import {v2 as cloudinary} from "cloudinary";
 
 import fs from "fs";
 
   
 cloudinary.config({ 
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-
-  api_key: process.env.CLOUDINARY_API_KEY, 
-
-  api_secret: process.env.CLOUDINARY_API_SECRET
-});
-
-
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET 
+  });
 const uploadoncloudinary=async(localfilepath)=>{
     try {
         if(!localfilepath){
