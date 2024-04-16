c74c22cd52ac140b7d0e181d59b507d46473beac
Dhruv Gandhi <dhruvgandhi870842@gmail.com>
2024-04-16 15:36:37 +0530

    watch history and routes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -435,6 +435,63 @@ const getUSerChannelProfile = asyncHandler(async(req, res) =>{
     )
 })
 
+
+const getWatchHistory= asyncHandler(async(req,res)=>{
+    const user= await User.aggregate([
+        {
+            $match:{
+                _id: new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup:{
+                from:"Video",
+                localField: "watchHistory",
+                foreignField: "_id",
+                as:"watchHistory",
+                pipeline:[
+                    {
+                        $lookup:{
+                            from:"users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+                            pipeline:[
+                                {
+                                    $project:{
+                                        fullName:1,
+                                        username:1,
+                                        avatar:1,
+                                    }
+                                }
+                            ]
+
+                        }
+                    },
+                    {
+                        $addFields:{
+                            owner:{
+                                $first:"$owner",
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ])
+
+    return res
+    .status(200) 
+    .json(
+        new ApiResponse(
+            200,
+            user[0].getWatchHistory,
+            "watch history fetched successfully"
+        )
+    )
+
+})
+
 export {
     registerUser, 
     loginuser, 
@@ -445,7 +502,8 @@ export {
     updateAccountDetails,
     updateAvatar,
     updateCoverImage,
-    getUSerChannelProfile
+    getUSerChannelProfile,
+    getWatchHistory
 
 }
 
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,26 +1,37 @@
-import {Router} from "express"
-import {registerUser} from "../controllers/user.controller.js"
-import {upload} from "../middlewares/multer.middleware.js"
-import { loginuser,logoutuser,refreshAccessToken } from "../controllers/user.controller.js"
-import{verifyJWT} from "../middlewares/auth.middleware.js"
+import { Router } from "express"
+import { 
+    changeCurrentPassword, 
+    registerUser, 
+    loginuser, 
+    logoutuser, 
+    refreshAccessToken, 
+    getCurrentUser, 
+    updateAccountDetails, 
+    updateAvatar, 
+    updateCoverImage, 
+    getUSerChannelProfile, 
+    getWatchHistory } from "../controllers/user.controller.js"
+import { upload } from "../middlewares/multer.middleware.js"
+
+import { verifyJWT } from "../middlewares/auth.middleware.js"
 
 const router = Router()
 
 router.route("/register").post(
     upload.fields([
         {
-            name:"avatar",
-            maxCount:2
+            name: "avatar",
+            maxCount: 2
 
         },
         {
-            name:"coverimage",
-            maxCount:2
+            name: "coverimage",
+            maxCount: 2
 
         }
     ]),
     registerUser
-    )
+)
 
 
 router.route("/login").post(loginuser)
@@ -28,5 +39,14 @@ router.route("/login").post(loginuser)
 router.route("/logout").post(verifyJWT, logoutuser)
 
 router.route("/refreshtoken").post(refreshAccessToken)
+router.route("change-password").post(verifyJWT, changeCurrentPassword)
+
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/update-account-details").patch(verifyJWT, updateAccountDetails)
+router.route("update-avatar").patch(verifyJWT, upload.single("avatar"), updateAvatar)
+router.route("/cover-image").patch(verifyJWT, upload.single("/coverimage"), updateCoverImage)
+router.route("/c/:username").get(verifyJWT, getUSerChannelProfile)
+router.route("/watch-history").get(verifyJWT, getWatchHistory)
+
 
 export default router
